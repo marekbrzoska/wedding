d9514b3e3024ca81aa8f59c82da969829f290143
Marek Brzóska <mbr@hp.(none)>
2011-10-23 21:49:21 +0200

    listing gifts

--- gifts/views.py
+++ gifts/views.py
@@ -1,6 +1,11 @@
 #-*- coding=utf-8 -*- 
 from django.shortcuts import render
 
+from wedding.gifts.models import Gift
+
 
 def home(request):
     return render(request, 'home.html')
+
+def gifts(request):
+    return render(request, 'gifts/list.html', {'gifts':Gift.objects.all()})
