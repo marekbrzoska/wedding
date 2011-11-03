8c724f94881a3ba8ba4f5d5da90d57cd8c1a7809
Marek Brzóska <mbr@hp.(none)>
2011-11-03 12:58:23 +0100

    Prettier tekst in admin (unicode funs for gifts and links)

--- gifts/models.py
+++ gifts/models.py
@@ -7,9 +7,13 @@ class Gift(models.Model):
     buyer_declaration_date = models.DateTimeField(blank=True, null=True, default=None)
 
     def __unicode__(self):
-        return self.name
+        buyer = u" ({0})".format(self.buyer) if self.buyer else ''
+        return self.name + buyer
 
 
 class Link(models.Model):
     gift = models.ForeignKey(Gift, related_name="links")
     href = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.gift.__unicode__() + u" {0}".format(self.href)
