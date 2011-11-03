9487b71abb9dbfcbd4ee40eac804c633f4c26e1b
Marek Brzóska <mbr@hp.(none)>
2011-11-03 13:10:32 +0100

    in Admin edit links within gift model page

--- gifts/admin.py
+++ gifts/admin.py
@@ -4,5 +4,17 @@ from django.contrib import admin
 
 from gifts.models import Gift, Link
 
-admin.site.register(Gift)
-admin.site.register(Link)
+
+
+class LinkInline(admin.StackedInline):
+    '''This class is to show Links inline (together with some other class)'''
+    model = Link
+    extra = 2
+
+class GiftAdmin(admin.ModelAdmin):
+    '''This adds Links to Gitf model page in admin'''
+    inlines = [LinkInline]
+
+
+#register GiftAdmin to represent Gift
+admin.site.register(Gift, GiftAdmin)
--- gifts/models.py
+++ gifts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Gift(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Gift')
     buyer = models.CharField(max_length=200, blank=True, null=True)
     buyer_declaration_date = models.DateTimeField(blank=True, null=True, default=None)
 
@@ -13,7 +13,7 @@ class Gift(models.Model):
 
 class Link(models.Model):
     gift = models.ForeignKey(Gift, related_name="links")
-    href = models.CharField(max_length=200)
+    href = models.CharField(max_length=200, verbose_name='example link')
 
     def __unicode__(self):
         return self.gift.__unicode__() + u" {0}".format(self.href)
